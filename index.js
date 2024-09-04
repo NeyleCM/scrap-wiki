f7a045cadfddb416483fcf93baf0f53f3797215b
NeyleCM <neylecmorantes@gmail.com>
2024-09-05 00:12:50 +0200

    conseguido-básico

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const url = 'https://es.wikipedia.org/wiki/Categor%C3%ADa:M%C3%BAsicos_de_rap'
 
 const server = http.createServer((req, res) => {
     //res.end('Hola Mundo');
-    axios.get(url).then((response) => {
+    axios.get(url, {
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        }
+    }).then((response) => {
         if(response.status === 200) {
         const html = response.data
         //console.log(html)
@@ -22,12 +26,38 @@ const server = http.createServer((req, res) => {
             imgs.push(img)
         })
         $('p').each((index, element) => {
-            const parrafo = $(element)
-            parrafo.push(parrafo)
+            const parrafo = $(element).text()
+            parrafos.push(parrafo)
         })
-    console.log(parrafos)
+    //console.log(parrafos)
+        res.writeHead(200, { 'Content-Type': 'text/html' }) 
+        res.write(
+            `<!DOCTYPE html>
+                <html lang="eS">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>${h1}</title>
+                </head>
+                    <body>
+                        <h2>Imágenes</h2>
+                            <ul>
+                                ${parrafos.map(parrafo => `<li><a href="${url}${parrafo}">${parrafo}</a></li>`).join('')}
+                            </ul>
+                        <h2>Párrafos</h2>
+                            <ul>
+                                ${imgs.map(img => `<li><a href="${url}${img}">${img}</a></li>`).join('')}
+                            </ul>
+                    </body>
+                </html>
+        `)
+        res.end();
         }    
-    });
+    }).catch((error) => {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Error fetching data');
+        console.error(error);
+    })
 })
   
   server.listen(3001, () => {
